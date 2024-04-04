87fff2892420b5decabb2623c85ba2e2d1ebbb3b
Mariyam Iqbal <[valid-email]>
2024-04-04 04:45:04 -0700

    first commit

---
+++ index.ts
@@ -0,0 +1,112 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+
+
+//interface done
+//array     done
+//function  dono
+//operators done
+
+
+//interface
+
+interface TodoItem {
+   task: string;
+   completed: boolean; 
+}
+
+//array
+
+const TodoList: TodoItem[] = [];
+
+//function
+
+ async function mainMenu() {
+    
+    const { action } = await inquirer.prompt({
+    type:'list',
+    name: 'action',
+   message: 'what would you like to do?',
+   choices: ['Add Task', 'View Task', 'Mark as Completed', 'Delete Task','Exit'],
+
+    });
+
+
+    switch (action)
+     {
+        case 'Add Task':
+          await addTask();
+            break;
+        case 'View Task':
+        viewTask();
+        break;
+        case 'Mark as Completed':
+            await markAsCompleted();
+            break;
+        case 'Delete Task':
+               await deleteTask();
+                break;
+
+        case 'Exit':
+            
+                    console.log("Good Bye!");
+           return;         
+        
+    }
+    mainMenu();
+  }
+    
+
+    let addTask = async () => {
+     let { task } = await inquirer.prompt({
+        type:'input',
+        name: 'task',
+        message: 'Enter the task',
+     });
+     TodoList.push({task,completed: false});
+     console.log("Task Added Successfully");
+    };
+
+   let viewTask = () => {
+    console.log("**** To Do List ****");
+    TodoList.forEach((item, index) => {
+      console.log(`${index + 1}.[${item.completed ? 'x' : ''}] ${item.task}`);
+        
+    });
+    console.log("************");
+    
+   }
+   let markAsCompleted = async () => {
+  let { index } = await inquirer.prompt({
+    type: 'number',
+    name: 'index',
+    message: 'Which task do you want to mark as completed?',
+  });
+  if(index <1 ||  index > TodoList.length){
+    console.log("Invalid task number. Please try again.");
+    return;
+  }
+
+  TodoList[index -1].completed = true;
+  console.log("Task mark as completed");
+  
+
+   }
+
+
+   let deleteTask = async () => {
+    let { index } = await inquirer.prompt({
+      type: 'number',
+      name: 'index',
+      message: 'Which task do you want to Deleted?',
+    });
+    if(index <1 ||  index > TodoList.length){
+      console.log("Invalid task number. Please try again.");
+      return;
+    }
+  
+    TodoList.splice(Math.floor(deleteTask.length/index -1));
+    console.log("Task Deleted Sucessfully");
+    
+     }
+     mainMenu();
